36f3e50d2acd7dcbf113b628825742e3f3da91c7
Sourav Nandi <souravnandi728@outlook.com>
2018-02-28 14:10:12 +0530

    editassets part added

--- app/src/main/java/com/example/tvd/assetstracking/HomeFragment.java
+++ app/src/main/java/com/example/tvd/assetstracking/HomeFragment.java
@@ -51,38 +51,4 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
         }
     }
-
-   /* public void fetchallrecord() {
-        Cursor cursor = database.getAssetsTecord();
-        if (cursor.getCount()>0) {
-            while (cursor.moveToNext())
-            {
-                GetSetValues getSetValues = new GetSetValues();
-                getSetValues.setId(String.valueOf(cursor.getInt(cursor.getColumnIndex("_id"))));
-                getSetValues.setProduct_id(cursor.getString(cursor.getColumnIndex("ITEM_NAME")));
-                getSetValues.setItem_name(cursor.getString(cursor.getColumnIndex("PRODUCT_ID")));
-                getSetValues.setBrand(cursor.getString(cursor.getColumnIndex("BRAND")));
-                getSetValues.setDate(cursor.getString(cursor.getColumnIndex("DATE")));
-                getSetValues.setDetails(cursor.getString(cursor.getColumnIndex("DETAILS")));
-                getSetValues.setCategory(cursor.getString(cursor.getColumnIndex("CATEGORY")));
-                getSetValues.setCompany(cursor.getString(cursor.getColumnIndex("COMPANY")));
-                getSetValues.setPrice(cursor.getString(cursor.getColumnIndex("PRICE")));
-                getSetValues.setQty(cursor.getString(cursor.getColumnIndex("QTY")));
-                getSetValues.setLocation(cursor.getString(cursor.getColumnIndex("LOCATION")));
-
-                Log.d("Debugg","ID"+cursor.getInt(cursor.getColumnIndex("_id")));
-                Log.d("Debugg","ITEM_NAME"+cursor.getString(cursor.getColumnIndex("ITEM_NAME")));
-                Log.d("Debugg","PRODUCT_ID"+cursor.getString(cursor.getColumnIndex("PRODUCT_ID")));
-                Log.d("Debugg","BRAND"+cursor.getString(cursor.getColumnIndex("DATE")));
-                Log.d("Debugg","DATE"+cursor.getString(cursor.getColumnIndex("DATE")));
-                Log.d("Debugg","DETAILS"+cursor.getString(cursor.getColumnIndex("DATE")));
-                Log.d("Debugg","CATEGORY"+cursor.getString(cursor.getColumnIndex("CATEGORY")));
-                Log.d("Debugg","COMPANY"+cursor.getString(cursor.getColumnIndex("COMPANY")));
-                Log.d("Debugg","PRICE"+cursor.getString(cursor.getColumnIndex("PRICE")));
-                Log.d("Debugg","QTY"+cursor.getString(cursor.getColumnIndex("QTY")));
-                Log.d("Debugg","LOCATION"+cursor.getString(cursor.getColumnIndex("LOCATION")));
-                arrayList.add(getSetValues);
-            }
-        }
-    }*/
 }
